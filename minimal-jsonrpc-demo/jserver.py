342a45394641f3c34b4ee02de88bb53454579739
Stephanie <smedina7@miners.utep.edu>
2018-11-13 00:49:35 -0700

    added to client and server to use json decode and encode

--- minimal-jsonrpc-demo/jserver.py
+++ minimal-jsonrpc-demo/jserver.py
@@ -6,21 +6,22 @@ from bsonrpc import JSONRpc
 from bsonrpc import request, service_class
 from bsonrpc.exceptions import FramingError
 from bsonrpc.framing import (
-	JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
+    JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
 
 
 # Class providing functions for the client to use:
 @service_class
 class ServerServices(object):
 
-  @request
-  def swapper(self, txt):
-    return ''.join(reversed(list(txt)))
+    @request
+    def swapper(self, txt):
+        return ''.join(reversed(list(txt)))
 
-  @request
-  def nop(self, txt):
-    print(txt)
-    return txt
+    @request
+    def nop(self, txt):
+        print(txt)
+        return txt
+
 
 # Quick-and-dirty TCP Server:
 ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,6 +29,6 @@ ss.bind(('localhost', 50001))
 ss.listen(10)
 
 while True:
-  s, _ = ss.accept()
-  # JSONRpc object spawns internal thread to serve the connection.
-  JSONRpc(s, ServerServices(),framing_cls=JSONFramingNone)
+    s, _ = ss.accept()
+    # JSONRpc object spawns internal thread to serve the connection.
+    JSONRpc(s, ServerServices(), framing_cls=JSONFramingNone)
