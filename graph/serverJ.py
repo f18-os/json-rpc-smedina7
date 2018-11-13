a808113315a04d2a7d5eb7520a42f70c89260f6f
Stephanie <smedina7@miners.utep.edu>
2018-11-12 23:25:42 -0700

    Testing

---
+++ graph/serverJ.py
@@ -0,0 +1,34 @@
+# minimalistic server example from
+# https://github.com/seprich/py-bson-rpc/blob/master/README.md#quickstart
+
+import socket
+from bsonrpc import JSONRpc
+from bsonrpc import request, service_class
+from bsonrpc.exceptions import FramingError
+from bsonrpc.framing import (
+    JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
+
+
+# Class providing functions for the client to use:
+@service_class
+class ServerServices(object):
+
+    @request
+    def swapper(self, txt):
+        return ''.join(reversed(list(txt)))
+
+    @request
+    def nop(self, txt):
+        print(txt)
+        return txt
+
+
+# Quick-and-dirty TCP Server:
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind(('localhost', 50001))
+ss.listen(10)
+
+while True:
+    s, _ = ss.accept()
+    # JSONRpc object spawns internal thread to serve the connection.
+    JSONRpc(s, ServerServices(), framing_cls=JSONFramingNone)
